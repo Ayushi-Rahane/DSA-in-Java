af0337a66f0834ac242f8aed136a9b7bba5fca0f
Ayushi-Rahane <ayushirahane2021@gmail.com>
2024-09-15 21:07:20 +0530

    c

---
+++ Operations on LinkedList/SSL_Operations.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class SSL_Operations {
+   public static void main(String[] args){
+        int c;
+        Scanner sc = new Scanner(System.in);
+        do{
+           System.out.println("Enter 1. to create linked list\n 2. to insert at first\n 3. to insert in between\n 4. to insert at end\n 5. to delete at start\n 6. to delete in between\n 7. to delete at end: ");
+           c = sc.nextInt();
+           switch(c){
+            case 1:
+               syso
+           }
+           
+
+            System.out.println("Enter 1 to continue: ");
+            c = sc.nextInt();
+            sc.nextLine();
+        }while(c==1);
+   }    
+}
